932b7c2bd7d3e5f40c327922fec20cc087cf516a
works-ito <h.ito@works-rental.co.jp>
2026-08-23 16:01:19 +0900

    Bump irregular category UI cache version

--- sales-stockin.js
+++ sales-stockin.js
@@ -1,4 +1,4 @@
-/* 販売品入庫受付 v97 bootstrap */
+/* 販売品入庫受付 v98 bootstrap */
 (function() {
   function loadScript(src) {
     return new Promise(function(resolve, reject) {
@@ -20,7 +20,7 @@
     .then(function() { return loadScript("./compact-scanner-dev.js?v=53"); })
     .then(function() { return loadScript("./irregular-master-picker-dev.js?v=65"); })
     .then(function() { return loadScript("./irregular-entry-simplify-dev.js?v=72"); })
-    .then(function() { return loadScript("./irregular-category-ui-tuning-dev.js?v=62"); })
+    .then(function() { return loadScript("./irregular-category-ui-tuning-dev.js?v=63"); })
     .then(function() { return loadScript("./irregular-simple-id-alias-dev.js?v=42"); })
     .then(function() { return loadScript("./irregular-master-layout-dev.js?v=40"); })
     .then(function() { return loadScript("./irregular-registration-guard-dev.js?v=43"); })
