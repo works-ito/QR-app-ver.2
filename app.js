7489b6dd4cb6297cea3cae06579bd7383b829d23
works-ito <h.ito@works-rental.co.jp>
2026-08-30 16:17:59 +0900

    app.js を更新

--- app.js
+++ app.js
@@ -3805,7 +3805,7 @@ function changePreviousSettings() {
 
       try {
         const profile = {
-          label:"3.5 Flash-Lite 全体1回",
+          label:"上半分1回",
           cropRatio:0.5,
           maxSide:1024,
           quality:0.75
